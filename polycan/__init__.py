45668ba6596f5e2e6b30ed863b16e4d137c533ef
Morgan Swanson <msswanson@Morgans-MacBook-Pro.local>
2018-12-09 00:06:10 -0800

    Fixed issue with authentication

--- polycan/__init__.py
+++ polycan/__init__.py
@@ -1,10 +1,11 @@
+import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import google.cloud.exceptions
 
 # Use the application default credentials
-cred = credentials.Certificate('secret.json')
+cred = credentials.Certificate(os.path.join(os.path.dirname(__file__), 'secret.json'))
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 input_prompt = ""
