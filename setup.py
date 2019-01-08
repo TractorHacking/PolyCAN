d7430584bf145d58b6baf3da34fc9b619ada2e15
Morgan Swanson <msswanson@Morgans-MacBook-Pro.local>
2019-01-08 11:57:32 -0800

    dependencies

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='polycan',
       author_email='msswanso@calpoly.edu',
       license='Open Source',
       packages=['polycan'],
+      install_requires=['tqdm','firebase-admin', 'tabulate',]
       zip_safe=False,
       entry_points={
         'console_scripts': [
