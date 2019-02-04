334214ffede099d87fa0d7d6031ff4cc5f839e3a
Morgan-Swanson <38450369+Morgan-Swanson@users.noreply.github.com>
2019-02-04 10:39:14 -0800

    Update setup.py
    Added Maskim's analysis dependancies.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='polycan',
       author_email='msswanso@calpoly.edu',
       license='Open Source',
       packages=['polycan'],
-      install_requires=['tqdm','firebase-admin', 'tabulate', 'grpcio'],
+      install_requires=['tqdm','firebase-admin', 'tabulate', 'grpcio', 'numpy', 'scipy', 'pandas', 'scikit-learn'],
       zip_safe=False,
       entry_points={
         'console_scripts': [
